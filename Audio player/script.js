11c36eaef913feacf4a122d19fc7a7717e0f1456
Tusharragarwal <159878817+Tusharragarwal@users.noreply.github.com>
2024-04-21 12:37:07 +0530

    Add files via upload

---
+++ Audio player/script.js
@@ -0,0 +1,245 @@
+
+    // video 
+    function playVideo() {
+        var video = document.getElementById("myVideo");
+        video.play();
+        }
+
+        function skipVideoOnClick(secimage, myVideo) {
+        
+          const imageElement = document.getElementById("secimage");
+          const videoElement = document.getElementById("myVideo");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick(secimage, myVideo) {
+
+          const imageElement = document.getElementById("secimage");
+          const videoElement = document.getElementById("myVideo");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        } 
+     
+    function pauseVideo() {
+      var video = document.getElementById("myVideo");
+      video.pause();
+    }
+
+     // video 1
+     function playVideo1() {
+        var video = document.getElementById("myVideo1");
+        video.play();
+        }
+
+        function skipVideoOnClick1(secimage1, myVideo1) {
+        
+          const imageElement = document.getElementById("secimage1");
+          const videoElement = document.getElementById("myVideo1");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick1(secimage1, myVideo1) {
+
+          const imageElement = document.getElementById("secimage1");
+          const videoElement = document.getElementById("myVideo1");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        }
+      
+    function pauseVideo1() {
+      var video = document.getElementById("myVideo1");
+      video.pause();
+    }
+
+     // video 2
+     function playVideo2() {
+        var video = document.getElementById("myVideo2");
+        video.play();
+        }
+
+        function skipVideoOnClick2(secimage2, myVideo2) {
+        
+          const imageElement = document.getElementById("secimage2");
+          const videoElement = document.getElementById("myVideo2");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick2(secimage2, myVideo2) {
+
+          const imageElement = document.getElementById("secimage2");
+          const videoElement = document.getElementById("myVideo2");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        }
+     
+    function pauseVideo2() {
+      var video = document.getElementById("myVideo2");
+      video.pause();
+    }
+
+     // video 3
+     function playVideo3() {
+        var video = document.getElementById("myVideo3");
+        video.play();
+        }
+      
+        function skipVideoOnClick3(secimage3, myVideo3) {
+        
+          const imageElement = document.getElementById("secimage3");
+          const videoElement = document.getElementById("myVideo3");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick3(secimage3, myVideo3) {
+
+          const imageElement = document.getElementById("secimage3");
+          const videoElement = document.getElementById("myVideo3");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        }
+    
+    function pauseVideo3() {
+      var video = document.getElementById("myVideo3");
+      video.pause();
+    }
+
+     // video 4
+     function playVideo4() {
+        var video = document.getElementById("myVideo4");
+        video.play();
+        }
+      
+        function skipVideoOnClick4(secimage4, myVideo4) {
+        
+          const imageElement = document.getElementById("secimage4");
+          const videoElement = document.getElementById("myVideo4");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick4(secimage4, myVideo4) {
+
+          const imageElement = document.getElementById("secimage4");
+          const videoElement = document.getElementById("myVideo4");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        }
+    
+    function pauseVideo4() {
+      var video = document.getElementById("myVideo4");
+      video.pause();
+    }
+
+     // video 5
+     function playVideo5() {
+        var video = document.getElementById("myVideo5");
+        video.play();
+        }
+
+        function skipVideoOnClick5(secimage5, myVideo5) {
+        
+          const imageElement = document.getElementById("secimage5");
+          const videoElement = document.getElementById("myVideo5");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick5(secimage5, myVideo5) {
+
+          const imageElement = document.getElementById("secimage5");
+          const videoElement = document.getElementById("myVideo5");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        } 
+   
+    function pauseVideo5() {
+      var video = document.getElementById("myVideo5");
+      video.pause();
+    }
+
+     // video 6
+     function playVideo6() {
+        var video = document.getElementById("myVideo6");
+        video.play();
+        }
+        function skipVideoOnClick6(secimage6, myVideo6) {
+        
+          const imageElement = document.getElementById("secimage6");
+          const videoElement = document.getElementById("myVideo6");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick6(secimage6, myVideo6) {
+
+          const imageElement = document.getElementById("secimage6");
+          const videoElement = document.getElementById("myVideo6");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        }
+   
+    function pauseVideo6() {
+      var video = document.getElementById("myVideo6");
+      video.pause();
+    }
+
+     // video 7
+     function playVideo7() {
+        var video = document.getElementById("myVideo7");
+        video.play();
+        }
+
+        function skipVideoOnClick7(secimage7, myVideo7) {
+        
+          const imageElement = document.getElementById("secimage7");
+          const videoElement = document.getElementById("myVideo7");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick7(secimage7, myVideo7) {
+
+          const imageElement = document.getElementById("secimage7");
+          const videoElement = document.getElementById("myVideo7");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        }    
+      
+    
+    function pauseVideo7() {
+      var video = document.getElementById("myVideo7");
+      video.pause();
+    }
+
+     // video 8
+     function playVideo8() {
+        var video = document.getElementById("myVideo8");
+        video.play();
+        }
+
+        function skipVideoOnClick8(secimage8, myVideo8) {
+        
+          const imageElement = document.getElementById("secimage8");
+          const videoElement = document.getElementById("myVideo8");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime + 10, videoElement.duration);
+        }  
+        
+        function reverseVideoOnClick8(secimage8, myVideo8) {
+
+          const imageElement = document.getElementById("secimage8");
+          const videoElement = document.getElementById("myVideo8");
+
+            videoElement.currentTime = Math.min(videoElement.currentTime - 10, videoElement.duration);
+        }
+      
+    
+    function pauseVideo8() {
+      var video = document.getElementById("myVideo8");
+      video.pause();
+    }
+  
